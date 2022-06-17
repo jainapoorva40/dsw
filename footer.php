86b0ec1e1e0116e1cc934be43293c8b399d27acd
Apoorva Jain <80565044+jainapoorva40@users.noreply.github.com>
2022-06-17 17:47:47 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+<div class="container-fluid animatedParent" style="background:#F8F9FA; padding:40px;"> 
+		    <div class="container">
+		      
+			      <div class="row animated fadeIn">
+				      
+					  
+					  <div class="col-sm-6" style=" border-right:1px solid black;">
+					  <h4>Developers</h4>
+  	 			<ul>
+  	 				<li> <a href="#">Aman Singh Rana</a></li>
+  	 				<li><a href="#">Apoorva Jain</a></li>
+  	 				<li><a href="#">Kartik gupta</a></li>
+  	 				<li><a href="#">Vaibhav JHA</a></li>
+  	 			</ul>
+
+					  </div>
+					  <div class="col-sm-6" style=" border-right:1px solid black;">
+					  <h4>Follow us on ! </h4>
+  	 			<ul>
+  	 				<li> <a href="#">Instagram</a></li>
+  	 				<li><a href="#">Facebook</a></li>
+  	 				<li><a href="#">Telegram</a></li>
+  	 				<li><a href="#">Linkedin</a></li>
+  	 			</ul>
+					  </div>
+				  </div>
+			 </div>
+			 			 
+		  </div>
